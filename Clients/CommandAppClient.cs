630e71e8df64af8d7c403a208f71c06530d9d14d
Bruno Maio <daigoro86dev@gmail.com>
2022-01-17 09:38:52 +0000

    First Commit

---
+++ Clients/CommandAppClient.cs
@@ -0,0 +1,24 @@
+﻿using RestSharp;
+
+namespace CommandAppClient.Clients;
+public static class ApiClients
+{
+    public async static Task<RestResponse> GetCommands()
+    {
+        var client = new RestClient($"{Environment.GetEnvironmentVariable("BASE_URL")}/api/commands");
+
+        var request = new RestRequest();
+
+        return await client.GetAsync(request);
+    }
+
+    public async static Task<RestResponse> GetCommandById(int id)
+    {
+        var client = new RestClient($"{Environment.GetEnvironmentVariable("BASE_URL")}/api/commands/{id}");
+
+        var request = new RestRequest();
+
+        return await client.GetAsync(request);
+    }
+
+}
